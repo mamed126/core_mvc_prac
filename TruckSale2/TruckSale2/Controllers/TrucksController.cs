ad1b8876b4cdbe49ec0bc78c5a4ea3db614b6a4b
mamed126 <mamed_126@mail.ru>
2021-09-14 13:06:36 +0400

    added trucksale mvc projects

---
+++ TruckSale2/TruckSale2/Controllers/TrucksController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TruckSale2.Models;
+
+namespace TruckSale2.Controllers
+{
+    public class TrucksController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Truck t)
+        {
+            if (ModelState.IsValid)
+            {
+                //do work
+                return RedirectToAction("Index");
+            }
+            return View(t);
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            Truck t = new Truck()
+            {
+                Brand = "Man",
+                Distributors = new System.Collections.Generic.List<string> { "Dist1", "Dist2" },
+                Id=1,
+                Model="TGA 450",
+                Price=150000,
+                RegisterDate=new System.DateTime(2021,1,1)
+            };
+
+            return View(t);
+        }
+    }
+}
---
+++ TruckSale2/TruckSale2/Models/Truck.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace TruckSale2.Models
+{
+    public class Truck
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Truck brand")]
+        [Required]
+        [StringLength(maximumLength:25,ErrorMessage ="Brand nam max length is 25 simvols")]
+        public string Brand { get; set; }
+
+        [Display(Name ="Model of truck")]
+        public string Model { get; set; }
+
+        [Display(Name ="Truck registered")]
+        [DataType(DataType.Date)]
+        public DateTime RegisterDate { get; set; }
+
+        public List<string> Distributors { get; set; }
+
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+
+    }
+}
