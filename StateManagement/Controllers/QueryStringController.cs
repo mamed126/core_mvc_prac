3690653e9be4078caf4d1958bf68e33eb1f87cba
mamed126 <mamed_126@mail.ru>
2021-09-15 12:54:47 +0400

    state management codes completed

---
+++ StateManagement/Controllers/QueryStringController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateManagement.Controllers
+{
+    public class QueryStringController : Controller
+    {
+        public IActionResult Index(string data)
+        {
+            return View("Index",data); 
+        }
+    }
+}
---
+++ StateManagement/Controllers/SessionDemoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StateManagement.Controllers
+{
+    public class SessionDemoController : Controller
+    {
+        private string _sessionKey = "name";
+        public IActionResult Index()
+        {
+           var data= HttpContext.Session.GetString(_sessionKey);
+
+            return View(nameof(Index),data);
+        }
+        [HttpPost]
+        public IActionResult Index(IFormCollection fc)
+        {
+            var data = fc["form_data"].ToString();
+            HttpContext.Session.SetString(_sessionKey, data);
+            return RedirectToAction(nameof(Index));
+        }
+        
+        public IActionResult RemoveSession()
+        {
+            HttpContext.Session.Clear();
+            return View(nameof(Index));
+        }
+    }
+}
