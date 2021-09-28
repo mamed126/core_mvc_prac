df34e0214d0066a53b5198ae5b4ac01509584306
mamed126 <mamed_126@mail.ru>
2021-09-28 17:38:54 +0400

    caching demo added

---
+++ CachingDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CachingDemo.Controllers
+{
+    [Route("Home/Index")]
+    public class HomeController : Controller
+    {
+        private IMemoryCache _cache;
+
+        public HomeController(IMemoryCache cache)
+        {
+            _cache = cache;
+        }
+
+        public IActionResult Index()
+        {
+            DateTime time;
+            bool dataCached = _cache.TryGetValue("time", out time);
+            if (!dataCached)
+            {
+                time = DateTime.Now;
+
+                MemoryCacheEntryOptions options =
+                    new MemoryCacheEntryOptions().SetSlidingExpiration(TimeSpan.FromSeconds(20));
+
+                _cache.Set("time", time, options);
+
+            }
+            return View(time);
+        }
+    }
+}
